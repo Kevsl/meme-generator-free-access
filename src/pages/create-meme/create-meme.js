5afa8ebdedf266c9c3f809f3f19ea004923bbdc2
Kevin Terrier <kevin.terrier@le1817.com>
2022-08-10 20:44:19 +0200

    successfully created meme

--- src/pages/create-meme/create-meme.js
+++ src/pages/create-meme/create-meme.js
@@ -1,26 +1,23 @@
 import { useSelector } from 'react-redux'
 import { getCreatedMeme } from '../../services/memes'
 import React, { useState } from 'react'
-import './create-meme.css'
 
 const CreateMeme = () => {
-  const url = useSelector((state) => state.meme.url)
+  const [url, setUrl] = useState(useSelector((state) => state.meme.url))
   const id = useSelector((state) => state.meme.id)
 
   const [text0, setText0] = useState('')
   const [text1, setText1] = useState('')
 
+  function handleSubmitMeme(id, text0, text1) {
+    getCreatedMeme(id, text0, text1).then((res) => {
+      setUrl(res)
+    })
+  }
+
   return (
     <div>
-      <div className="relative">
-        <p className="absolute top-20 text-center w-50 fs-32 text-white">
-          {text0}
-        </p>
-        <img src={url} alt="login-meme" className="w-50 br-9 block" />
-        <p className="absolute bottom-20 text-center w-50 fs-32 text-white">
-          {text1}
-        </p>
-      </div>
+      <img src={url} alt="login-meme" className="w-50 br-9 block" />
       <h1 className="text-center">Create your meme</h1>
 
       <input
@@ -41,6 +38,15 @@ const CreateMeme = () => {
           setText1(e.target.value)
         }}
       />
+
+      <button
+        className="block w-50 bg-lightGray br-9 text-center btn-border bg-blue"
+        onClick={() => {
+          handleSubmitMeme(id, text0, text1)
+        }}
+      >
+        Create your meme
+      </button>
     </div>
   )
 }
--- src/services/memes.js
+++ src/services/memes.js
@@ -6,3 +6,20 @@ export async function getMemes() {
     .get('https://api.imgflip.com/get_memes')
     .then((response) => response.data.data.memes)
 }
+export async function getCreatedMeme(id, text0, text1) {
+  let myFormData = new FormData()
+  myFormData.append('template_id', id)
+  myFormData.append('username', process.env.REACT_APP_USERNAME)
+  myFormData.append('password', process.env.REACT_APP_PASSWORD)
+  myFormData.append('text0', text0)
+  myFormData.append('text1', text1)
+
+  let axiosConfig = {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  }
+  return axios
+    .post('https://api.imgflip.com/caption_image', myFormData, axiosConfig)
+    .then((response) => response.data.data.url)
+}
