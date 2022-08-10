a5aa9a7edec8087f7af2e8a01a3dab68efaf2d5b
Kevin Terrier <kevin.terrier@le1817.com>
2022-08-10 11:28:02 +0200

    added security

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css'
 import Login from './pages/login/login'
 import Registration from './pages/login/registration'
+import React, { useEffect } from 'react'
 
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
+  const token = localStorage.getItem('accessToken')
+  const currentLocation = window.location.pathname
+
+  useEffect(() => {
+    if (!token || token === 'undefined') {
+      if (currentLocation !== '/login') {
+        window.location.replace('/login')
+      }
+    }
+  }, [currentLocation, token])
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
       </Routes>
     </div>
