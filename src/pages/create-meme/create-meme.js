f7d549ae193ddaa0c88a3301479258007bace537
Kevin Terrier <kevin.terrier@le1817.com>
2022-08-11 16:49:44 +0200

    made some corrections

--- src/pages/create-meme/create-meme.js
+++ src/pages/create-meme/create-meme.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux'
 import { getCreatedMeme } from '../../services/memes'
 import React, { useState } from 'react'
+import './create-meme.css'
 
 const CreateMeme = () => {
   const [url, setUrl] = useState(useSelector((state) => state.meme.url))
@@ -16,12 +17,12 @@ const CreateMeme = () => {
   }
 
   return (
-    <div>
-      <img src={url} alt="login-meme" className="w-50 br-9 block" />
+    <div className="w-50">
+      <img src={url} alt="login-meme" className=" br-9 block mw-100" />
       <h1 className="text-center">Create your meme</h1>
 
       <input
-        className="block w-50 bg-lightGray indent-10 br-9"
+        className="block mb-10 w-100   bg-lightGray indent-10 br-9"
         type="text"
         value={text0}
         placeholder="Texte du haut"
@@ -30,7 +31,7 @@ const CreateMeme = () => {
         }}
       />
       <input
-        className="block w-50 bg-lightGray indent-10 br-9"
+        className="block mb-10 w-100   bg-lightGray indent-10 br-9"
         type="text"
         value={text1}
         placeholder="Texte du bas"
@@ -40,7 +41,7 @@ const CreateMeme = () => {
       />
 
       <button
-        className="block w-50 bg-lightGray br-9 text-center btn-border bg-blue"
+        className="block mb-10 w-100  bg-lightGray br-9 text-center btn-border bg-blue"
         onClick={() => {
           handleSubmitMeme(id, text0, text1)
         }}
